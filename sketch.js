cfce316ea9487c897888bf1714379b3fd51cb7ff
Aarjav Chauhan <aarjavchauhan439@gmail.com>
2019-09-21 11:30:54 -0600

    draw connecting lines and color

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
+//key from mapbox account
 const key = 'pk.eyJ1IjoiYWFyamF2YyIsImEiOiJjazBwbGkzd2swNnRvM2xxbGUyd3J5d3p3In0.g-pcOPmMcYV48Bnx99NODA'
 
+//map options created online on mapbox
 const options = {
   lat: 28.6,
   lng: 77.200000,
@@ -13,39 +15,65 @@ const mappa = new Mappa('Mapbox', key);
 let myMap;
 let canvas;
 
+//array storing pixel locations of viewable capitals
 let capitalArray = [];
+
+let connectionLines = 2
 
 function preload() {
   data = loadJSON('country_capitals.json')
 }
 
 function setup() {
-  canvas = createCanvas(500, 500);
-
+  canvas = createCanvas(700, 700);
   // Create a tile map and overlay the canvas on top.
   myMap = mappa.tileMap(options);
   myMap.overlay(canvas);
-
   // Only redraw the meteorites when the map change and not every frame.
   myMap.onChange(mapCapitals);
-
   //  drawCapitals();
-  fill(255, 255, 255);
-  noStroke()
+//  fill(255, 255, 255);
+//  noStroke()
 }
 
 function draw() {
-clear();
-for (var i = 0; i < capitalArray.length; i++) {
+  clear();
+  drawCircles()
+  drawConnections(capitalArray)
+}
+
+function drawCircles(){
+  push()
+  noStroke()
+  fill(125, 125, 125)
+  for (var i = 0; i < capitalArray.length; i++) {
     var latitude = capitalArray[i][0];
     var longitude = capitalArray[i][1];
     var radius = 10 + 10 * sin(frameCount / 50);
     circle(latitude, longitude, radius)
-    }
+  }
+  pop()
 }
 
+function drawConnections(pointsArray){
+  push()
+  stroke(random(255)* sin(frameCount / 5), random(255)* sin(frameCount / 5), random(255)* sin(frameCount / 5))
+  for (var i = 0; i < pointsArray.length; i++) {
+    if (pointsArray.length > 1 && i < pointsArray.length-1) {
+      line(pointsArray[i][0], pointsArray[i][1], pointsArray[i+1][0], pointsArray[i+1][1])
+    }else if (pointsArray.length > 2 && i == pointsArray.length-1) {
+      line(pointsArray[i][0], pointsArray[i][1], pointsArray[0][0], pointsArray[0][1])
+    }
+  }
+  console.log()
+  pop()
+}
 
-function mapCapitals() {
+//read JSON file
+//get the latitudes and longitudes of the capitals
+//convert and map them to pixels on the canvas
+//store them in the capitalArray
+function mapCapitals(){
 
   capitalArray.length = 0;
 
@@ -55,7 +83,6 @@ function mapCapitals() {
     if (myMap.map.getBounds().contains([latitude, longitude])) {
       const position = myMap.latLngToPixel(latitude, longitude);
       capitalArray.push([position.x, position.y]);
-      //ellipse(position.x, position.y, 10, 10)
     }
   }
 }
